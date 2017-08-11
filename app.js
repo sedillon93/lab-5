e9ebe1c6d2669fee2c262863ea60d574dfe59a8a
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-11 14:56:04 -0700

    finish revising problem 3

--- app.js
+++ app.js
@@ -47,19 +47,24 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+//DONE
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var sumResult = sum(a,b);
-  var finalSum = sum(sumResult,c);
-  var productResult = multiply(a,b);
-  var finalProduct = multiply(productResult,c);
-  var sumOutput = a + ' and ' + b + ' and ' + c + ' sum to ' + finalSum + '.';
-  var productOutput = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + finalProduct + '.';
-  return [finalSum, finalProduct, sumOutput, productOutput];
+  var sumResult = sum(a,b);;
+  //sumResult = [sum of a + b, message(see above)]
+  var finalSum = sum(sumResult[0],c);;
+  //finalSum = [sum of sumResul + c, message]
+  console.log(finalSum);
+  var productResult = multiply(a,b);;
+  //productResult = [product of a * b, message(see above)]
+  var finalProduct = multiply(productResult[0],c);;
+  //finalProduct = [product of productResult*c, message]
+  var sumOutput = a + ' and ' + b + ' and ' + c + ' sum to ' + finalSum[0] + '.';
+  var productOutput = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + finalProduct[0] + '.';
+  return [finalSum[0], finalProduct[0], sumOutput, productOutput];
 }
 
-// TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+//DONE
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
