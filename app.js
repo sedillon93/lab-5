5be0929f5b8549fc5f41cf55d0de21213258c711
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-11 13:56:50 -0700

    rewrite TODOs to DONEs for problem 2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE
 function multiply(a,b){ //eslint-disable-line
   var product = a * b;
   var output = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
@@ -34,7 +34,7 @@ function multiply(a,b){ //eslint-disable-line
   console.log(output);
 }
 
-// TODO: Here is the test for multiply(); uncomment it to run it
+// DONE
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
