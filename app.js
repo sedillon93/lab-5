f1e957319bcc3ccadbacfd39ee43b5824325e5c4
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-17 08:33:06 -0700

    remove excess console logs

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ function multiply(a,b){ //eslint-disable-line
   var product = a * b;
   var message = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
   return [product, message];
-  console.log(product);
-  console.log(output);
 }
 
 // DONE
@@ -53,7 +51,6 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
   //sumResult = [sum of a + b, message(see above)]
   var finalSum = sum(sumResult[0],c);;
   //finalSum = [sum of sumResul + c, message]
-  console.log(finalSum);
   var productResult = multiply(a,b);;
   //productResult = [product of a * b, message(see above)]
   var finalProduct = multiply(productResult[0],c);;
@@ -98,19 +95,17 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
-// Write your code here
+// DONE
 function multiplyArray(testArray){ //eslint-disable-line
   var totalProduct = multiply(testArray[0], testArray[1])[0];
-  console.log(totalProduct);
   for (var i = 2; i < testArray.length; i++) {
     totalProduct = multiply(testArray[i],totalProduct)[0];
   }
-  console.log(totalProduct);
   var message = 'The numbers ' + testArray.join(',') + ' have a product of ' + totalProduct + '.';
   return [totalProduct, message];
 }
 
-// Here is the test for multiplyArray(); uncomment it to run it
+// DONE
 testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
